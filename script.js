10419bcacf09b1c162ced624e0719ff487282028
Marlette Dakiwas <mjessdakiwas@gmail.com>
2025-08-20 00:48:32 -0400

    refactored js

--- script.js
+++ script.js
@@ -17,75 +17,23 @@
     await fetchData();
     const productsArray = fetchedData.products; // accessing products array inside the json file
 
-    // fetching json using .then() chain
-    // fetch('products.json')
-    //     .then(response => {
-    //         if (!response.ok) {
-    //             throw new Error(`HTTP error--Status: ${response.status}`);
-    //         }
-    //         return response.json() // parse response as JSON
-    //     })
-    //     .then(data => {
-    //         fetchedData = data;
-    //         productsArray = fetchedData.products; // accessing products array inside the json file
-    //     })
-    //     .catch(error => {
-    //         console.error('Error fetching JSON:', error);
-    //     })
-    // console.log(fetchedData);
-    // console.log(productsArray);
-
-    ///// Notes /////
-
-    // QUESTIONS:
-    // 1. What is the difference between fetching JSON data through an async function and through fetch().then()
-
-    // Fetch operation is asynchronous, meaning that it'll execute before data is available 
-    // The fetchData variable must be acessed after the promised has resolved 
-    // (i.e., inside the async function after await or within the .then() block where it's assigned)
-
-    ///// End of Notes /////
-
     // this function will allow me to have more control on how many product elements I want to create
     // needed this in the cases where I only display certain products (e.g., ice cream of the month, best sellers)
     function createProductElement(product) {
-        const productContainer = document.createElement('div');
+        // WARNING: Using template literals to create elements pose security issues
+        const productContainer = `
+            <div class="product-container">
+                <div class="product-img__container">
+                    <img class="product-img" src="${product.image}" alt="${product.name}">
+                </div>
+                <div class="product-info__container">
+                    <p class="product-name">${product.name}</p>
+                    <p class="product-price">$${product.price}</p>
+                    <p class="product-description">${product.description}</p>
+                    <button class="add-to-cart__button">Add to Cart</button>
+                </div>
+            </div>`
 
-        const productImgContainer = document.createElement('div');
-        const productImg = document.createElement('img');
-
-        const productInfoContainer = document.createElement('div');
-        const productName = document.createElement('p');
-        const productPrice = document.createElement('p');
-        const productDescription = document.createElement('p');
-        const addToCartButton = document.createElement('button');
-
-        productContainer.classList.add('product-container');
-        productImgContainer.classList.add('product-img__container');
-        productImg.classList.add('product-img');
-
-        productInfoContainer.classList.add('product-info__container');
-        productName.classList.add('product-name');
-        productPrice.classList.add('product-price');
-        productDescription.classList.add('product-description');
-        addToCartButton.classList.add('add-to-cart__button');
-
-        productImg.src = product.image;
-        productImg.alt = product.name;
-        productName.textContent = product.name;
-        productPrice.textContent = `$${product.price}`;
-        productDescription.textContent = product.description;
-        addToCartButton.textContent = `Add to Cart`;
-
-        productImgContainer.appendChild(productImg);
-
-        productInfoContainer.appendChild(productName);
-        productInfoContainer.appendChild(productPrice);
-        productInfoContainer.appendChild(productDescription);
-        productInfoContainer.appendChild(addToCartButton);
-
-        productContainer.appendChild(productImgContainer);
-        productContainer.appendChild(productInfoContainer);
         return productContainer;
     }
 
@@ -94,29 +42,15 @@
     // function used to populate all the products inside the json file (for the products page)
     function populateProducts(products, productContainerParent) {
         for (const product of products) {
-            // console.log(product);
             const productContainer = createProductElement(product);
-            // console.log(productContainer);
-            productContainerParent.appendChild(productContainer);
+            productContainerParent.innerHTML += productContainer;
         }
     }
 
     // https://www.geeksforgeeks.org/javascript/how-to-get-a-value-from-a-json-array-in-javascript/ => different methods to get values from a json array
     function monthlyFeatureDisplay(productNames) {
         const monthlyFeatureContainer = document.getElementById('monthly-feature__container');
-
-        let monthlyFeatureProducts = [];
-        for (const productName of productNames) {
-            for (const product of productsArray) {
-                console.log(productName);
-                console.log(product.name);
-                if (product.name === productName) {
-                    monthlyFeatureProducts.push(product);
-                }
-            }
-        }
-        console.log(monthlyFeatureProducts);
-
+        const monthlyFeatureProducts = productsArray.filter((product) => productNames.includes(product.name))
         populateProducts(monthlyFeatureProducts, monthlyFeatureContainer);
     }
 
@@ -124,18 +58,12 @@
     // productNamesArray is an array of products that will pass as the best sellers
     function bestSellersDisplay(productNames) {
         const bestSellersContainer = document.getElementById('best-sellers');
-        let bestSellersProducts = []; // contains the objects of the passed best sellers products array
-        for (const productName of productNames) {
-            // finding the product objects of best sellers products
-            const productObj = productsArray.find(product => product.name === productName);
-            bestSellersProducts.push(productObj);
-        }
-
+        const bestSellersProducts = productsArray.filter((product) => productNames.includes(product.name))
         populateProducts(bestSellersProducts, bestSellersContainer);
     }
 
-
-    if (window.location.pathname === '/index.html') {
+    // to refactor/further optimize code, will create separate js files for each html file requiring it
+    if (window.location.pathname === '/index.html' || window.location.pathname === '/') {
         monthlyFeatureDisplay(['Pineapple Twist-Up']);
         bestSellersDisplay(['Flip-Top Float', 'Jukebox Jive', 'Maple Jitterbug', 'Malt Shop Magic']);
     }
@@ -150,6 +78,7 @@
     const closeMenuBtn = document.getElementById('mobile-nav__close');
     const navModal = document.getElementById('mobile-nav');
 
+    // to refactor/further optimize code, will make .hidden class to alter display
     openMenuBtn.addEventListener('click', () => {
         navModal.style.display = getComputedStyle(navModal).display === 'none' ? 'flex' : 'none';
         openMenuBtn.style.display = 'none';
@@ -172,81 +101,57 @@
         const emailInput = document.getElementById('email');
         const phoneNumInput = document.getElementById('phone-number');
         const messageInput = document.getElementById('message');
-        const messageSubmitInput = document.getElementById('contact-form__submit');
         const messageSent = document.getElementById('message-sent');
         const messageSentReturnBtn = document.getElementById('message-return__button')
-        const invalidFnameMsg = document.getElementById('invalid-fname__msg');
-        const invalidLnameMsg = document.getElementById('invalid-lname__msg');
-        const invalidEmailMsg = document.getElementById('invalid-email__msg');
-        const invalidPhoneMsg = document.getElementById('invalid-phonenum__msg');
-        const invalidMessageMsg = document.getElementById('invalid-msg__msg');
 
         const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
         const phoneNumRegex = /^(\+\d{1,2}\s?)?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}$/;
 
+        let isValid = true;
+
+        const invalid = (input) => {
+            function message() {
+                if (input.value === '') {
+                    const inputDataName = input.getAttribute('data-name');
+                    return `${inputDataName} is empty. Please enter a valid value.`
+                } else {
+                    if (input === emailInput) {
+                        return "Email address is in an incorrect format. Please enter a valid value containing @.";
+                    } else if (input === phoneNumInput) {
+                        return `Phone number is in an incorrect format. Please enter any valid phone number format (ex. 123-456-7890, (123) 456-7890).`
+                    }
+                }
+            }
+
+            input.classList.add('invalid-input');
+            // const invalidMsg = `<p class="invalid-input__msg" id="invalid-${input.name}__msg">${message()}</p>`; // not possible to use .after() for this case as the method typically inserts strings as text, even if it's a template literal creating a node
+            const invalidMsg = document.createElement('p');
+            invalidMsg.classList.add('invalid-input__msg');
+            invalidMsg.id = `invalid-${input.name}__msg`
+            invalidMsg.textContent = message();
+            input.after(invalidMsg);
+            isValid = false;
+        }
+
+        const valid = (input) => {
+            const invalidMsg = document.getElementById(`invalid-${input.name}__msg`);
+            if (document.contains(invalidMsg)) {
+                console.log(`removing: ${invalidMsg}`);
+                invalidMsg.remove();
+            }
+            input.classList.remove('invalid-input');
+        }
+
         contactForm.addEventListener('submit', function (event) {
             event.preventDefault();
 
-            let isValid = true;
-            if (fnameInput.value === '') {
-                invalidFnameMsg.style.display = 'block';
-                invalidFnameMsg.textContent = "First name is empty. Please enter a valid value."
-                fnameInput.classList.add('invalid-input');
-                isValid = false;
-            } else {
-                invalidFnameMsg.style.display = 'none';
-                fnameInput.classList.remove('invalid-input');
-            }
-
-            if (lnameInput.value === '') {
-                invalidLnameMsg.style.display = 'block';
-                invalidLnameMsg.textContent = "Last name is empty. Please enter a valid value."
-                lnameInput.classList.add('invalid-input');
-                isValid = false;
-            } else {
-                invalidLnameMsg.style.display = 'none';
-                lnameInput.classList.remove('invalid-input');
-            }
-
-            if (emailInput.value === '') {
-                invalidEmailMsg.style.display = 'block';
-                invalidEmailMsg.textContent = "Email address is empty. Please enter a valid value."
-                emailInput.classList.add('invalid-input');
-                isValid = false;
-            } else if (!emailRegex.test(emailInput.value)) {
-                invalidEmailMsg.style.display = 'block';
-                invalidEmailMsg.textContent = "Email address is in an incorrect format. Please enter a valid value."
-                emailInput.classList.add('invalid-input');
-                isValid = false;
-            } else {
-                invalidEmailMsg.style.display = 'none';
-                emailInput.classList.remove('invalid-input');
-            }
-
-            if (phoneNumInput.value === '') {
-                invalidPhoneMsg.style.display = 'block';
-                invalidPhoneMsg.textContent = "Phone number is empty. Please enter a valid value."
-                phoneNumInput.classList.add('invalid-input');
-                isValid = false;
-            } else if (!phoneNumRegex.test(phoneNumInput.value)) {
-                invalidPhoneMsg.style.display = 'block';
-                invalidPhoneMsg.textContent = "Phone number is in an incorrect format. Please enter a valid value."
-                phoneNumInput.classList.add('invalid-input');
-                isValid = false;
-            } else {
-                invalidPhoneMsg.style.display = 'none';
-                phoneNumInput.classList.remove('invalid-input');
-            }
-
-            if (messageInput.value === '') {
-                invalidMessageMsg.style.display = 'block';
-                invalidMessageMsg.textContent = "Phone number is empty. Please enter a valid value."
-                messageInput.classList.add('invalid-input');
-                isValid = false;
-            } else {
-                invalidMessageMsg.style.display = 'none';
-                messageInput.classList.remove('invalid-input');
-            }
+            const inputs = document.querySelectorAll('input');
+            inputs.forEach((input) => {
+                const invalidMsg = document.getElementById(`invalid-${input.name}__msg`);
+                if (!document.contains(invalidMsg)) {
+                    !input.value || !emailRegex.test(emailInput.value) || !phoneNumRegex.test(phoneNumInput.value) ? invalid(input) : valid(input);
+                }
+            });
 
             if (isValid) {
                 messageSent.style.display = 'flex';
@@ -260,30 +165,24 @@
             contactForm.style.display = 'flex';
         })
 
-        fnameInput.addEventListener('change', () => {
-            invalidFnameMsg.style.display = 'none';
-            fnameInput.classList.remove('invalid-input');
+        fnameInput.addEventListener('input', (event) => {
+            valid(event.target); // event.target gets 'fnameInput'
         })
 
-        lnameInput.addEventListener('change', () => {
-            invalidLnameMsg.style.display = 'none';
-            lnameInput.classList.remove('invalid-input');
+        lnameInput.addEventListener('input', (event) => {
+            valid(event.target);
         })
 
-        emailInput.addEventListener('change', () => {
-            invalidEmailMsg.style.display = 'none';
-            emailInput.classList.remove('invalid-input');
+        emailInput.addEventListener('input', (event) => {
+            valid(event.target);
         })
 
-        phoneNumInput.addEventListener('change', () => {
-            invalidPhoneMsg.style.display = 'none';
-            phoneNumInput.classList.remove('invalid-input');
+        phoneNumInput.addEventListener('input', (event) => {
+            valid(event.target);
         })
 
-        messageInput.addEventListener('change', () => {
-            invalidMessageMsg.style.display = 'none';
-            messageInput.classList.remove('invalid-input');
+        messageInput.addEventListener('input', (event) => {
+            valid(event.target);
         })
     }
 })();
-
